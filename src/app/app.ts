4d44cb8aa9e636d547b824b4ad92a93b7c484fc7
Admin <vasyanovichk@gmail.com>
2026-02-27 20:26:22 +0200

    fix-02

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('github-actions-angular-'));
+  protected readonly title = signal('github-actions-angular-');
 }
